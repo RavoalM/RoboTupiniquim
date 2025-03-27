2bcd87aae77880b01e556b0d0a3bd2f5d9c0a1b7
Alvaro Machado <alvarofeltrin2006@uniplaclages.edu.br>
2025-03-27 17:50:37 -0300

    Criação do "menu" e estruturação das variaveis

--- RoboTupiniquim.ConsoleApp/Program.cs
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Informe o limite do campo de pesquisa: ");
+            char[] Limite = Console.ReadLine().Replace(" ", "").ToCharArray();
+
+            int xLimite = int.Parse(Limite[0].ToString());
+            int yLimite = int.Parse(Limite[1].ToString());
+
+            Console.Write("Informe o spawn do robo (use o modelo 1 2 N): ");
+            char[] spawn = Console.ReadLine().Replace(" ", "").ToUpper().ToCharArray();
+
+            int xInicial = int.Parse(spawn[0].ToString());
+            int yInicial = int.Parse(spawn[1].ToString());
+            char direcaoInicial = spawn[2];
+
+            Console.Write("Informe o comando: ");
+            char[] listaComando = Console.ReadLine().Replace(" ", "").ToUpper().ToCharArray();
+
         }
+
     }
 }
