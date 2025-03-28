35bad9c0a3eaa54ed318ff441445c80640b1645a
Alvaro Machado <alvarofeltrin2006@uniplaclages.edu.br>
2025-03-28 12:03:30 -0300

    Conclusão da logica de movimentação

--- RoboTupiniquim.ConsoleApp/Program.cs
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            Console.Clear();
             Console.Write("Informe o limite do campo de pesquisa: ");
             char[] Limite = Console.ReadLine().Replace(" ", "").ToCharArray();
 
@@ -19,6 +20,89 @@
 
             Console.Write("Informe o comando: ");
             char[] listaComando = Console.ReadLine().Replace(" ", "").ToUpper().ToCharArray();
+
+
+            foreach (char comando in listaComando)
+            {
+                if (direcaoInicial == 'N')
+                {
+                    if (comando == 'D')
+                    {
+                        direcaoInicial = 'L';
+                    }
+                    if (comando == 'E')
+                    {
+                        direcaoInicial = 'O';
+                    }
+                }
+
+                else if (direcaoInicial == 'S')
+                {
+                    if (comando == 'D')
+                    {
+                        direcaoInicial = 'O';
+                    }
+                    if (comando == 'E')
+                    {
+                        direcaoInicial = 'L';
+                    }
+                }
+
+                else if (direcaoInicial == 'O')
+                {
+                    if (comando == 'D')
+                    {
+                        direcaoInicial = 'N';
+                    }
+                    if (comando == 'E')
+                    {
+                        direcaoInicial = 'S';
+                    }
+                }
+
+                else if (direcaoInicial == 'L')
+                {
+                    if (comando == 'D')
+                    {
+                        direcaoInicial = 'S';
+                    }
+                    if (comando == 'E')
+                    {
+                        direcaoInicial = 'N';
+                    }
+                }
+
+                if (comando == 'M')
+                {
+                    if (direcaoInicial == 'N')
+                    {
+                        yInicial++;
+                    }
+                    else if (direcaoInicial == 'S')
+                    {
+                        yInicial--;
+                    }
+                    else if (direcaoInicial == 'O')
+                    {
+                        xInicial--;
+                    }
+                    else if (direcaoInicial == 'L')
+                    {
+                        xInicial++;
+                    }
+
+                }
+            }
+
+            if (xInicial < 0 || xInicial > xLimite || yInicial < 0 || yInicial > yLimite)
+            {
+                Console.WriteLine("O robo saiu do campo de pesquisa");
+            }
+            else
+            {
+
+                Console.WriteLine($"Posição final do robo: {xInicial} {yInicial} {direcaoInicial}");
+            }
 
         }
 
